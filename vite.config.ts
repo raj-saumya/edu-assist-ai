d74d05dae663103fc4663dd2550c7b6a8cf55b1d
saumya raj <saumya.raj@gmail.com>
2025-09-26 23:55:11 +0530

    updated deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   server: {
@@ -16,6 +17,7 @@ export default defineConfig({
       start: { entry: './start.tsx' },
       server: { entry: './server.ts' },
     }),
+    netlify(),
     viteReact(),
   ],
 })
